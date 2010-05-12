6eb467d4441d6262849b5ee0b6a1cb99de32c3c0
Miek Gieben <miek@miek.nl>
2010-05-12 15:12:57 +0200

    added

--- ex-todo/ex-calc.go
+++ ex-todo/ex-calc.go
@@ -1,8 +1,13 @@
 \begin{Exercise}[title={Calculator},difficulty=7]
 \label{ex:calc}
-\Question \label{ex:calc q1} Create a simple stack which can hold a
+\Question \label{ex:calc q1} Create a simple \emph{reverse polish calculator}. Such a calculator
+accept input like \texttt{5 11 +} and then print \texttt{16}; so first the operands and then the
+operation. For now it only needs to support addition with two operands. Just like in the
+example given.
 
-\Question \label{ex:calc q2} 
+You will need your stack implementation from exercise \ref{ex:stack}.
+
+\Question \label{ex:calc q2} Extend your calculator to support substraction (-) and multiplication (*).
 \end{Exercise}
 
 \begin{Answer}
