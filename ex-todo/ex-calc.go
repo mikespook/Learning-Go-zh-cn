ed51bb170dd4f02b4872c9aab68b9365902a70d6
Miek Gieben <miek@miek.nl>
2010-05-12 12:36:33 +0200

    add more stuff

---
+++ ex-todo/ex-calc.go
@@ -0,0 +1,14 @@
+\begin{Exercise}[title={Calculator},difficulty=7]
+\label{ex:calc}
+\Question \label{ex:calc q1} Create a simple stack which can hold a
+
+\Question \label{ex:calc q2} 
+\end{Exercise}
+
+\begin{Answer}
+
+\Question
+
+\Question
+
+\end{Answer}
