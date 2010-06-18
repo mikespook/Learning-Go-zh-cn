63102920e682458cea550e376c210c0543f4b7dc
Miek Gieben <miek@miek.nl>
2010-06-18 22:44:55 +0200

    move fib function around. From:Sonia Keys

---
+++ ex-functions/src/fib.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func fibonacci(value int) []int {
+    x := make([]int, value)
+    x[0], x[1] = 1, 1
+    for n := 2; n < value; n++ {
+        x[n] = x[n-1] + x[n-2]
+    }
+    return x
+}
+
+func main() {
+    for _, term := range fibonacci(10) {
+        fmt.Println(term)
+    }
+}
