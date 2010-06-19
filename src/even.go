4b38a6402508c476f06d9447655f860fb5c521ef
Miek Gieben <miek@miek.nl>
2010-06-19 20:43:41 +0200

    add comments

--- src/even.go
+++ src/even.go
@@ -1,13 +1,13 @@
-package even
+package even	|\coderemark{Our own namespace}|
 
-func Even(i int) bool {
+func Even(i int) bool { |\coderemark{Exported}|
 	if i % 2 == 0 {
 		return true
 	}
 	return false
 }
 
-func uneven(i int) bool {
+func uneven(i int) bool { |\coderemark{Private}|
 	if i % 2 != 0 {
 		return false
 	}
