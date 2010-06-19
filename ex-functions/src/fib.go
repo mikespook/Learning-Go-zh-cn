7fcac65409e3e577c419f9cf1eb32ad85f049099
Miek Gieben <miek@miek.nl>
2010-06-19 08:36:34 +0200

    Move around the fibonaci exercise and add one in channels chapter

--- ex-functions/src/fib.go
+++ ex-functions/src/fib.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 func fibonacci(value int) []int {
-    x := make([]int, value)
-    x[0], x[1] = 1, 1
-    for n := 2; n < value; n++ {
-        x[n] = x[n-1] + x[n-2]
-    }
-    return x
+	x := make([]int, value)
+	x[0], x[1] = 1, 1
+	for n := 2; n < value; n++ {
+		x[n] = x[n-1] + x[n-2]
+	}
+	return x
 }
 
 func main() {
-    for _, term := range fibonacci(10) {
-        fmt.Println(term)
-    }
+	for _, term := range fibonacci(10) {
+		fmt.Printf("%v ", term)
+	}
 }
