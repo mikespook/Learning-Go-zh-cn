d7bf88285a3856199e68660ca43604b30af75a32
Miek Gieben <miek@miek.nl>
2010-06-26 00:55:49 +0200

    a lot better text. Chapter 2 is also getting better

--- src/types.go
+++ src/types.go
@@ -1,8 +1,8 @@
 package main
 
 func main() {
-	var a int
-	var b int32
+	var a int |\coderemark{generic integer type}|
+	var b int32 |\coderemark{32 bits integer type}|
 	a = 15
-	b = a + a |\coderemark{illegal mixing of types}|
+	b = a + a |\coderemark{illegal mixing of these types}|
 }
