69c7aaa251e6be1b54012680a9eccca4652563a0
Miek Gieben <miek@miek.nl>
2010-07-06 18:26:18 +0200

    more

--- src/reflect.go
+++ src/reflect.go
@@ -1,23 +1,14 @@
 package main
 
-type PersonAge struct { |\coderemark{First we define two structures as a new type, %
+type PersonAge struct { |\longremark{First we define two structures as a new type, %
 \texttt{PersonAge};}|
 	name string
 	age  int
 }
 
-type PersonShoe struct { |\coderemark{And \texttt{PersonShoe};}|
+type PersonShoe struct { |\longremark{And \texttt{PersonShoe};}|
 	name     string
 	shoesize int
-}
-
-func WhichOne(x interface{}) {
-	switch t := x.(type) {
-	case *PersonAge:
-		println("Age person")
-	case *PersonShoe:
-		println("Shoe person")
-	}
 }
 
 func main() {
@@ -26,3 +17,15 @@ func main() {
 	WhichOne(p1)
 	WhichOne(p2)
 }
+
+func WhichOne(x interface{}) { |\longremark{This function must accept \emph{both} %
+types as valid input, so we use the empty interface, which every type implements;}|
+	switch t := x.(type) { |\longremark{The type switch;}|
+	case *PersonAge:	|\longremark{When allocated with \func{new} its a %
+pointer;}|
+		println("Age person")
+	case *PersonShoe:
+		println("Shoe person")
+	}
+}
+
