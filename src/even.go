50179685c2d1496b8dccb6abc3cd34e1a977b240
Miek Gieben <miek@miek.nl>
2010-07-08 00:57:56 +0200

    positioning of coderemakr

--- src/even.go
+++ src/even.go
@@ -1,13 +1,13 @@
-package even	|\coderemark{Our own namespace}|
+package even			|\coderemark{Our own namespace}|
 
-func Even(i int) bool { |\coderemark{Exported}|
+func Even(i int) bool {		|\coderemark{Exported}|
 	if i % 2 == 0 {
 		return true
 	}
 	return false
 }
 
-func uneven(i int) bool { |\coderemark{Private}|
+func uneven(i int) bool {	|\coderemark{Private}|
 	if i % 2 != 0 {
 		return false
 	}
--- src/types.go
+++ src/types.go
@@ -1,8 +1,8 @@
 package main
 
 func main() {
-	var a int |\coderemark{generic integer type}|
-	var b int32 |\coderemark{32 bits integer type}|
+	var a int	|\coderemark{generic integer type}|
+	var b int32	|\coderemark{32 bits integer type}|
 	a = 15
-	b = a + a |\coderemark{illegal mixing of these types}|
+	b = a + a	|\coderemark{illegal mixing of these types}|
 }
