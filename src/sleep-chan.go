7b261721c0d2855fa3c1255a55d28ecb8a793480
Miek Gieben <miek@miek.nl>
2010-08-10 20:48:03 +0200

    fix double page bib layout

--- src/sleep-chan.go
+++ src/sleep-chan.go
@@ -5,14 +5,19 @@ import (
 	"fmt"
 )
 
+var c chan int
+
 func ready(w string, sec int) {
 	time.Sleep(int64(sec) * 1e9)
 	fmt.Println(w, "is ready!")
+	c <- 1
 }
 
 func main() {
+	c = make(chan int)
 	go ready("Tee", 2)
 	go ready("Coffee", 1)
-	fmt.Println("I'm waiting")
-	time.Sleep(5 * 1e9)
+	fmt.Println("I'm waiting, but not too long")
+	<-c
+	<-c
 }
