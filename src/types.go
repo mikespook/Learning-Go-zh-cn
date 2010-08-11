45b9866d99162a3e4221201fc764cf0a039bdf89
Miek Gieben <miek@miek.nl>
2010-08-12 01:00:41 +0200

    More comments from JC

--- src/types.go
+++ src/types.go
@@ -5,4 +5,5 @@ func main() {
 	var b int32	|\coderemark{32 bits integer type}|
 	a = 15
 	b = a + a	|\coderemark{Illegal mixing of these types}|
+	b = b + 5	|\coderemark{5 is a (typeless) constant, so this is OK}|
 }
