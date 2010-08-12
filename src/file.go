4974c24b32aead885ea2e047eae55f76a607e726
Miek Gieben <miek@miek.nl>
2010-08-12 12:11:47 +0200

    add reading and writing from files

---
+++ src/file.go
@@ -0,0 +1,16 @@
+package main
+
+import "os"
+
+func main() {
+	buf := make([]byte, 1024)
+	f, _ := os.Open("/etc/passwd", os.O_RDONLY, 0666)
+	defer f.Close()
+	for {
+		n, _ := f.Read(buf)
+		if n == 0 {
+			break
+		}
+		os.Stdout.Write(buf[0:n])
+	}
+}
