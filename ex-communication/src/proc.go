101f25ae28055e7bdbd369c8e6741304f9164d9e
Miek Gieben <miek@miek.nl>
2010-08-13 11:25:29 +0200

    add difficult exercise

--- ex-communication/src/proc.go
+++ ex-communication/src/proc.go
@@ -28,28 +28,29 @@ func main() {
 	r := bufio.NewReader(pr)
 	w := bufio.NewWriter(os.Stdout)
 	defer w.Flush()
-
-	pid, _ := os.ForkExec("/bin/ps", []string{"ps", "-e", "-opid,ppid,comm"}, nil, "",
-		[]*os.File{nil, pw, nil})
+	// Capture the output from `ps`
+	pid, _ := os.ForkExec("/bin/ps", []string{"ps", "-e", "-opid,ppid,comm"}, nil, "", []*os.File{nil, pw, nil})
 	defer os.Wait(pid, os.WNOHANG)
 	pw.Close()
 
-	child := make(map[int]*vector.IntVector)	// strings that are numbers (pid)
-	r.ReadString('\n')		// header
+	child := make(map[int]*vector.IntVector)
+	r.ReadString('\n')	// Discard the header line
 	for {
 		s, ok := r.ReadString('\n')
 		if ok != nil {
 			break
 		}
-		f := strings.Fields(s)
+		f := strings.Fields(s)	 // Split the line
 
 		if _, present := child[atoi(f[PPID])]; !present {
 			v := new(vector.IntVector)
 			child[atoi(f[PPID])] = v
 		}
+		// Save the child PIDs on a vector
 		child[atoi(f[PPID])].Push(atoi(f[PID]))
 	}
 
+	// Sort the PPIDs
 	schild := make([]int, len(child))
 	i := 0
 	for k, _ := range child {
@@ -57,10 +58,10 @@ func main() {
 		i++
 	}
 	sort.SortInts(schild)
-
+	// Walk throught the sorted list
 	for _, ppid := range schild {
 		pids := child[ppid]
-		fmt.Printf("Pid %d has %d child",
+		fmt.Printf("Pid %d has %d child",   // Print them
 			ppid, pids.Len())
 
 		if pids.Len() == 1 {
--- ex-communication/src/proc.pl
+++ ex-communication/src/proc.pl
@@ -3,13 +3,13 @@ use strict;
 use warnings;
 
 my (%child, $pid, $parent);
-my @ps=`ps -e -opid,ppid,comm`;
-foreach (@ps[1..$#ps]) {
-    ($pid, $parent, undef) = split;
-    push @{$child{$parent}}, $pid;
+my @ps=`ps -e -opid,ppid,comm`;	    # Capture the ouput from `ps`
+foreach (@ps[1..$#ps]) {	    # Discard the header line
+    ($pid, $parent, undef) = split; # Split the line, discard 'comm'
+    push @{$child{$parent}}, $pid;  # Save the child PIDs on a list
 }
-
-foreach (sort { $a <=> $b } keys %child) {
-    print "Pid ", $_, " has ", @{$child{$_}}+0, " child", 
+# Walk through the sorted PPIDs
+foreach (sort { $a <=> $b } keys %child) {  
+    print "Pid ", $_, " has ", @{$child{$_}}+0, " child",  # Print them
 	@{$child{$_}} == 1 ? ": " : "eren: ", "@{$child{$_}}";
 }
