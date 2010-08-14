f672ca033135766a798e040bc9cddcc5637144bd
Miek Gieben <miek@miek.nl>
2010-08-14 22:23:53 +0200

    add average exercise an answer

---
+++ ex-unsorted/src/ave.go
@@ -0,0 +1,19 @@
+package main
+
+func average(xs []float) (ave float) {
+	sum := 0
+	switch len(xs) {
+	case 0:
+		ave = 0
+	default:
+		for _, v := range xs {
+			sum += v
+		}
+		ave = sum / len(xs)
+	}
+	return
+}
+
+func main() {
+
+}
