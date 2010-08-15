5b0f3f4dd6c59e05ad953262f310b730438a9b3d
Miek Gieben <miek@miek.nl>
2010-08-15 10:07:25 +0200

    better map function

--- ex-functions/src/map.go
+++ ex-functions/src/map.go
@@ -1,30 +1,31 @@
 package main
+
 import "fmt"
 
 /* define the empty interface as a type */
 type e interface{}
 
 func mult2(f e) e {
-    switch f.(type) {
-    case int:
-        return f.(int) * 2
-    case string:
-        return f.(string) + f.(string) + f.(string) + f.(string)
-    }
-    return f
+	switch f.(type) {
+	case int:
+		return f.(int) * 2
+	case string:
+		return f.(string) + f.(string) + f.(string) + f.(string)
+	}
+	return f
 }
 
 func Map(n []e, f func(e) e) {
-    for k, v := range n {
-        n[k] = f(v)
-    }
+	for k, v := range n {
+		n[k] = f(v)
+	}
 }
 
 func main() {
-    m := [...]e{1, 2, 3, 4}
-    s := [...]e{"a", "b", "c", "d"}
-    Map(&m, mult2)
-    Map(&s, mult2)
-    fmt.Printf("%v\n", m)
-    fmt.Printf("%v\n", s)
+	m := []e{1, 2, 3, 4}
+	s := []e{"a", "b", "c", "d"}
+	Map(m, mult2)
+	Map(s, mult2)
+	fmt.Printf("%v\n", m)
+	fmt.Printf("%v\n", s)
 }
