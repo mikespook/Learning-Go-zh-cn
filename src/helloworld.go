f08356bc2d56be276d86a8f291f92c213a1eab73
Miek Gieben <miek@miek.nl>
2010-08-15 09:01:23 +0200

    drop the nonquoted import

--- src/helloworld.go
+++ src/helloworld.go
@@ -1,6 +1,6 @@
 package main
 
-import fmt "fmt"  // Implements formatted I/O.
+import "fmt"  // Implements formatted I/O.
 
 func main() {
  fmt.Printf("Hello, world; or |$\kappa\alpha\lambda\eta\mu\acute{\epsilon}\rho\alpha\hspace{1em}\kappa $\'o$ \sigma\mu\epsilon$|; or|\begin{cjk}こんにちは 世界\end{cjk}|\n")
