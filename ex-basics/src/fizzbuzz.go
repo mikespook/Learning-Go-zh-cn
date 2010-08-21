820c9bc94909d02ac671e73eabed0d36fe253c16
Miek Gieben <miek@miek.nl>
2010-08-21 11:21:15 +0200

    first batch of comments, checked in

--- ex-basics/src/fizzbuzz.go
+++ ex-basics/src/fizzbuzz.go
@@ -3,23 +3,23 @@ package main
 import "fmt"
 
 func main() {
-        const (
-                FIZZ = 3
-                BUZZ = 5
-        )
-        for i := 1; i < 100; i++ {
-                p := false
-                if i%FIZZ == 0 {
-                        fmt.Printf("Fizz")
-                        p = true
-                }
-                if i%BUZZ == 0 {
-                        fmt.Printf("Buzz")
-                        p = true
-                }
-                if !p {
-                        fmt.Printf("%v", i)
-                }
-                fmt.Println()
-        }
+	const (
+		FIZZ = 3
+		BUZZ = 5
+	)
+	for i := 1; i < 100; i++ {
+		p := false
+		if i%FIZZ == 0 {
+			fmt.Printf("Fizz")
+			p = true
+		}
+		if i%BUZZ == 0 {
+			fmt.Printf("Buzz")
+			p = true
+		}
+		if !p {
+			fmt.Printf("%v", i)
+		}
+		fmt.Println()
+	}
 }
