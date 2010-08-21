3c81ee640e3dcca1606a653e3214719a34c57a48
Miek Gieben <miek@miek.nl>
2010-08-21 09:58:08 +0200

    style remarks from Ben Bullock

--- src/helloworld.go
+++ src/helloworld.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"  // Implements formatted I/O.
 
+/* Print something */
 func main() {
  fmt.Printf("Hello, world; or |$\kappa\alpha\lambda\eta\mu\acute{\epsilon}\rho\alpha\hspace{1em}\kappa $\'o$ \sigma\mu\epsilon$|; or|\begin{cjk}こんにちは 世界\end{cjk}|\n")
 }
