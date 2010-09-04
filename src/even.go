98f4a4c184a651c656a03ce2719a609a869ac71c
Miek Gieben <miek@miek.nl>
2010-09-04 22:12:07 +0200

    korter!

--- src/even.go
+++ src/even.go
@@ -1,15 +1,9 @@
 package even		|\coderemark{Start our own namespace}|
 
 func Even(i int) bool {	|\coderemark{Exported}|
-	if i % 2 == 0 {
-		return true
-	}
-	return false
+	return i % 2 == 0
 }
 
 func odd(i int) bool {	|\coderemark{Private}|
-	if i % 2 != 0 {
-		return false
-	}
-	return true
+	return i % 2 == 1
 }
