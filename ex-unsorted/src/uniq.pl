30ab64bc90c9c21959fe110cbc12da1f74f21ded
Miek Gieben <miek@miek.nl>
2010-10-13 20:41:09 +0200

    exercise fixes

--- ex-unsorted/src/uniq.pl
+++ ex-unsorted/src/uniq.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl
 my @a = qw/a b a a a c d e f g/;
-my $last = shift @a; print $last;
+my $first = shift @a; print $first;
 
 foreach (@a) {
-    if ($last ne $_) {
+    if ($first ne $_) {
 	print $_;
-	$last = $_;
+	$first = $_;
     }
 }
