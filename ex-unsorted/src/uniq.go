28f450028b82231d41bff3fbff2059defa1de22b
Miek Gieben <miek@miek.nl>
2010-10-13 20:41:01 +0200

    exercise fixes

---
+++ ex-unsorted/src/uniq.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	list := []string{"a", "b", "a", "a", "c", "d", "e", "f"}
+
+	//	fmt.Printf("%v\n", list)
+	first := list[0]
+	fmt.Printf("%s ", first)
+	for _, v := range list[1:] {
+		if first != v {
+			fmt.Printf("%s ", v)
+			first = v
+		}
+	}
+
+}
