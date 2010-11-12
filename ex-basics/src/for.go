dea6c886a5bce5f3ff25299452ce0f8f4e368b7a
Miek Gieben <miek@miek.nl>
2010-11-12 23:58:46 +0100

    fixes

--- ex-basics/src/for.go
+++ ex-basics/src/for.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 10; i++ { |\coderemark{See section \nameref{sec:for}}|
+	for i := 0; i < 10; i++ { |\coderemark{See section \titleref{sec:for}}|
 		fmt.Printf("%d\n", i)
 	}
 }
