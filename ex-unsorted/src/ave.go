5ea7e22c44e21a39e0c7c9e7eb3900b9aff05840
Miek Gieben <miek@miek.nl>
2011-01-31 19:40:18 +0100

    extra average function

--- ex-unsorted/src/ave.go
+++ ex-unsorted/src/ave.go
@@ -1,19 +1,16 @@
 package main
 
-func average(xs []float32) (ave float32) {
+func average(xs []float64) (ave float64) { |\longremark{We use a named return parameter;}|
 	sum := 0.0
 	switch len(xs) {
-	case 0:
+	case 0:                 |\longremark{Is the length is zero, we return 0;}|
 		ave = 0
-	default:
+	default:                |\longremark{Otherwise we calculate the average;}|
 		for _, v := range xs {
 			sum += v
 		}
-		ave = sum / float32(len(xs))
+		ave = sum / float64(len(xs)) |\longremark{We have to convert the value to a
+\key{float64} to make the division work;}|
 	}
-	return
-}
-
-func main() {
-	/* ... */
+	return  |\longremark{We have an avarage, return it.}|
 }
--- ex-unsorted/src/wc.go
+++ ex-unsorted/src/wc.go
@@ -9,13 +9,15 @@ import (
 
 func main() {
 	var chars, words, lines int
-	r := bufio.NewReader(os.Stdin)
+	r := bufio.NewReader(os.Stdin) |\longremark{Start a new reader that reads from standard input;}|
 	for {
-		switch s, ok := r.ReadString('\n'); true {
-		case ok != nil:
+		switch s, ok := r.ReadString('\n'); true { |\longremark{Read a line from the input;}|
+		case ok != nil: |\longremark{If we received an error, we assume it was because %
+of a EOF. So we print the current values;}|
 			fmt.Printf("%d %d %d\n", chars, words, lines);
 			return
-		default:
+		default:        |\longremark{Otherwise we count the charaters, words and increment the %
+lines.}|
 			chars += len(s)
 			words += len(strings.Fields(s))
 			lines++
