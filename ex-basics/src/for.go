1e848fe48650f183599cce576c4bdc6f233cf3a1
Miek Gieben <miek@miek.nl>
2011-02-01 20:27:23 +0100

    more tweaks to the first 2 chapters

--- ex-basics/src/for.go
+++ ex-basics/src/for.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 10; i++ { |\coderemark{See section \titleref{sec:for}}|
+	for i := 0; i < 10; i++ { |\coderemark{See section \titleref{sec:for} on page %
+\pageref{sec:for}}|
 		fmt.Printf("%d\n", i)
 	}
 }
