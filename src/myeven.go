146a87547449cbc86379fb1b7a7b178b9c4a5ae5
Miek Gieben <miek@miek.nl>
2011-02-01 21:05:19 +0100

    fixes for chapter 4

--- src/myeven.go
+++ src/myeven.go
@@ -1,13 +1,13 @@
 package main
 
 import (		|\longremark{Import the following packages;}|
-	"./even"	|\longremark{The \emph{local} package \package{even};}|
-	"fmt"		|\longremark{The official \package{fmt} package;}|
+	"./even"	|\longremark{The \emph{local} package \package{even} is imported here;}|
+	"fmt"		|\longremark{The official \package{fmt} package gets imported;}|
 )
 
 func main() {
 	i := 5
 	fmt.Printf("Is %d even? %v\n", i, even.Even(i)) |\longremark{Use the function from the %
 \package{even} package. The syntax for accessing a function from a package is %
-\lstinline{<package>.Function}.}|
+\lstinline{<package>.Function()}.}|
 }
