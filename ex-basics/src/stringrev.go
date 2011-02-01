21e3390911d0132e3fcff7ee5a2921c6aa48d60c
Miek Gieben <miek@miek.nl>
2011-02-01 20:55:35 +0100

    another bunch of fixex, chapter 3

--- ex-basics/src/stringrev.go
+++ ex-basics/src/stringrev.go
@@ -7,7 +7,7 @@ func main() {
 	a := []byte(s) |\coderemark{Again a conversion}|
 	// Reverse a
 	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
-		a[i], a[j] = a[j], a[i]
+		a[i], a[j] = a[j], a[i] |\coderemark{Parallel assignment}|
 	}
 	fmt.Printf("%s\n", string(a)) |\coderemark{Convert it back}|
 }
