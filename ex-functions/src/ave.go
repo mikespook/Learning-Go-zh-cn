f6c393d0d4c7a9086aee3bdd8ac7c11d0e8d3ba1
Miek Gieben <miek@miek.nl>
2011-03-04 22:41:36 +0100

    Another bunch of fixes from: Filip Zaludek

--- ex-functions/src/ave.go
+++ ex-functions/src/ave.go
@@ -3,7 +3,7 @@ package main
 func average(xs []float64) (ave float64) { |\longremark{We use a named return parameter;}|
 	sum := 0.0
 	switch len(xs) {
-	case 0:                 |\longremark{Is the length is zero, we return 0;}|
+	case 0:                 |\longremark{If the length is zero, we return 0;}|
 		ave = 0
 	default:                |\longremark{Otherwise we calculate the average;}|
 		for _, v := range xs {
