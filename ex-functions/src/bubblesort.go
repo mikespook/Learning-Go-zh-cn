e0288b46ba37df4f04801696ce5a2933b20b9ab8
Miek Gieben <miek@miek.nl>
2011-03-05 10:43:38 +0100

    Filip Zaludek: slightly more efficient bubblesort

--- ex-functions/src/bubblesort.go
+++ ex-functions/src/bubblesort.go
@@ -12,7 +12,7 @@ func main() {
 }
 
 func bubblesort(n []int) {
-	for i := 0; i < len(n); i++ {
+	for i := 0; i < len(n) - 1; i++ {
 		for j := i + 1; j < len(n); j++ {
 			if n[j] < n[i] {
 				n[i], n[j] = n[j], n[i]
