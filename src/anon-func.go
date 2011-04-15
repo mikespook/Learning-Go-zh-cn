36b8bec78679bd032dc0cd197ffb70c0e9117a64
mikespook <none@none>
2011-04-15 12:09:03 +0800

    finished the chapter 3

--- src/anon-func.go
+++ src/anon-func.go
@@ -1,8 +1,8 @@
 package main
 
 func main() {
-	a := func() {     |\coderemark{Define a nameless function and assign to \var{a}}|
+	a := func() {     |\coderemark{定义一个匿名函数，并且赋值给 \var{a}}|
 	  println("Hello")
-	}		  |\coderemark{No () here}|
-	a()               |\coderemark{Call the function}|
+	}		  |\coderemark{这里没有 ()}|
+	a()               |\coderemark{调用函数}|
 }
