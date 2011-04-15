f41fbceee6fdce0c8a11818064ebc435bfe6b6be
mikespook <none@none>
2011-04-15 17:31:07 +0800

    exercises for the chapter 3.

--- ex-functions/src/ave.go
+++ ex-functions/src/ave.go
@@ -1,16 +1,15 @@
 package main
 
-func average(xs []float64) (ave float64) { |\longremark{We use a named return parameter;}|
+func average(xs []float64) (ave float64) { |\longremark{可以使用命名返回值；}|
 	sum := 0.0
 	switch len(xs) {
-	case 0:                 |\longremark{If the length is zero, we return 0;}|
+	case 0:                 |\longremark{如果长度是零，返回 0；}|
 		ave = 0
-	default:                |\longremark{Otherwise we calculate the average;}|
+	default:                |\longremark{否则，计算平均值；}|
 		for _, v := range xs {
 			sum += v
 		}
-		ave = sum / float64(len(xs)) |\longremark{We have to % 
-convert the value to a \key{float64} to make the division work;}|
+		ave = sum / float64(len(xs)) |\longremark{为了使除法能正常计算，必须将值转换为 \key{float64}；}|
 	}
-	return  |\longremark{We have an avarage, return it.}|
+	return  |\longremark{得到平均值，返回它}|
 }
--- ex-functions/src/fib.go
+++ ex-functions/src/fib.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 func fibonacci(value int) []int {
-	x := make([]int, value) |\longremark{We create an \key{array} to hold the integers up to the value given in the function call;}|
-	x[0], x[1] = 1, 1 |\longremark{Starting point of the Fibonicai calculation;}|
+	x := make([]int, value) |\longremark{创建一个用于保存函数执行结果的 \key{array}；}|
+	x[0], x[1] = 1, 1 |\longremark{开始计算斐波那契数列；}|
 	for n := 2; n < value; n++ {
 		x[n] = x[n-1] + x[n-2] |\longremark{$x_n = x_{n-1} + x_{n-2}$;}|
 	}
-	return x |\longremark{Return the \emph{entire} array;}|
+	return x |\longremark{返回\emph{整个} array；}|
 }
 
 func main() {
-	for _, term := range fibonacci(10) { |\longremark{Using the \key{range} keyword we "walk" the numbers returned by the fibonacci funcion. Here up to 10. And we print them.}|
+	for _, term := range fibonacci(10) { |\longremark{使用保留字 \key{range} 可以逐个得到斐波那契函数返回的序列。这里有 10 个。并且打印出来了。}|
 		fmt.Printf("%v ", term)
 	}
 }
