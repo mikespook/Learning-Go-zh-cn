634be4b2c6fce0189542e6c6bba2e2546ae4ce5e
Miek Gieben <miek@miek.nl>
2011-07-03 12:03:42 +0200

    Add more stuff and make line parsing explicit

--- src/buffile.go
+++ src/buffile.go
@@ -1,17 +1,24 @@
 package main
-
 import ( "os"; "bufio")
 
 func main() {
 	buf := make([]byte, 1024)
-	f, _ := os.Open("/etc/passwd", os.O_RDONLY, 0666)
+	f, _ := os.Open("/etc/passwd", os.O_RDONLY, 0666) |\longremark{Open the file, \key{os.Open} returns %
+a \type{*os.File} which implements the \type{io.Reader} interface;}|
 	defer f.Close()
-	r := bufio.NewReader(f)
+	r := bufio.NewReader(f) |\longremark{Turn \var{f} into a buffered Reader.%
+\func{NewReader} expects an \type{io.Reader}, so you %
+might think this will fail. But it does not. %
+\emph{Anything} that has such a \func{Read()} function implements this %
+interface. And from listing \ref{src:read} we can see %
+that \type{*File} indeed does so;}|
 	w := bufio.NewWriter(os.Stdout)
 	defer w.Flush()
 	for {
-		n, _ := r.Read(buf)
+		n, _ := r.Read(buf)     |\longremark{Read from the Reader and write to the Writer, and thus %
+print the file to the screen.}|
 		if n == 0 { break }
 		w.Write(buf[0:n])
 	}
 }
+
--- src/file.go
+++ src/file.go
@@ -1,14 +1,13 @@
 package main
-
 import "os"
 
 func main() {
 	buf := make([]byte, 1024)
-	f, _ := os.Open("/etc/passwd", os.O_RDONLY, 0666)
-	defer f.Close()
+	f, _ := os.Open("/etc/passwd", os.O_RDONLY, 0666) |\longremark{Open the file;}|
+	defer f.Close() |\longremark{Make sure we close it again;}|
 	for {
-		n, _ := f.Read(buf)
-		if n == 0 { break }
-		os.Stdout.Write(buf[0:n])
+		n, _ := f.Read(buf) |\longremark{Read up to 1023 bytes at the time;}|
+		if n == 0 { break } |\longremark{We have reached the end of the file;}|
+		os.Stdout.Write(buf[0:n]) |\longremark{Write the contents to \var{Stdout};}|
 	}
 }
