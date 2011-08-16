5400c2fef6f290566d8c24636ba487eb6018e755
Miek Gieben <miek@miek.nl>
2011-08-16 20:29:50 +0200

    Fixes in chapter 3

--- ex-functions/src/bubblesort.go
+++ ex-functions/src/bubblesort.go
@@ -1,12 +1,9 @@
 package main
-
 import "fmt"
 
 func main() {
 	n := []int{5, -1, 0, 12, 3, 5}
 	fmt.Printf("unsorted %v\n", n)
-	// even though it's call by value, a slice is a
-	// reference type, so the underlaying array is changed!
 	bubblesort(n)
 	fmt.Printf("sorted %v\n", n)
 }
