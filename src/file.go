b7436f94b931904af357e1ffd7a7cf09acec4b0d
mikespook <none@none>
2011-08-23 22:25:46 +0800

    sync with English version '2011-07-03'

--- src/file.go
+++ src/file.go
@@ -6,9 +6,13 @@ func main() {
 	buf := make([]byte, 1024)
 	f, _ := os.Open("/etc/passwd", os.O_RDONLY, 0666)
 	defer f.Close()
+
+        f, _ := os.Open("/etc/passwd", os.O_RDONLY, 0666) |\longremark{打开文件；}|
+        defer f.Close() |\longremark{确保关闭（close）它；}|
+
 	for {
-		n, _ := f.Read(buf)
-		if n == 0 { break }
-		os.Stdout.Write(buf[0:n])
+                n, _ := f.Read(buf) |\longremark{一次读取 1023 字节；}|
+                if n == 0 { break } |\longremark{到达文件末尾；}|
+                os.Stdout.Write(buf[0:n]) |\longremark{将内容写入 \var{Stdout}；}|
 	}
 }
