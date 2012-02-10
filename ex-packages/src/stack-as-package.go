e90793c512df7287ba80a374f1034c93aba86244
Miek Gieben <miek@miek.nl>
2012-02-10 12:07:03 +0100

    Document the package

--- ex-packages/src/stack-as-package.go
+++ ex-packages/src/stack-as-package.go
@@ -1,15 +1,18 @@
 package stack
 
+// Stack holds the items.
 type Stack struct {
 	i    int
 	data [10]int
 }
 
+// Push pushes an item on the stack.
 func (s *Stack) Push(k int) {
 	s.data[s.i] = k
 	s.i++
 }
 
+// Pop pops an item from the stack.
 func (s *Stack) Pop() (ret int) {
 	s.i--
 	ret = s.data[s.i]
