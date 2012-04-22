a97ad0213f48cea4d751ca7a03bd9ef6bab95eeb
mikespook <mikespook@gmail.com>
2012-04-22 21:55:13 +0800

    make words formated.

--- src/helloworld.go
+++ src/helloworld.go
@@ -1,8 +1,8 @@
-package main |\longremark{首行这个是必须的。所有的 Go 文件以 \lstinline{package <something>}
-开头，对于独立运行的执行文件必须是 \lstinline{package main}；}|
+package main |\longremark{首行这个是必须的。所有的~Go 文件以~\lstinline{package <something>}
+开头，对于独立运行的执行文件必须是~\lstinline{package main}；}|
 
-import "fmt"  // 实现格式化的 I/O。 \longremark{这是说需要将 \package{"fmt"} 包加入 %
-\package{main}。不是 \package{main} 的其他包都被称为库，其他许多编程语言有着类似的概念（参阅第 %
+import "fmt"  // 实现格式化的~I/O。\longremark{这是说需要将~\package{"fmt"} 包加入~\package{main}。%
+不是~\package{main} 的其他包都被称为库，其他许多编程语言有着类似的概念（参阅第~%
 \ref{chap:packages} 章）。末尾以 \lstinline|//| 开头的内容是注释；}
 
 /* Print something */ |\longremark{这同样是注释，不过这是被包裹于 \lstinline|/*| 和 \lstinline|*/| 之间的；}|
