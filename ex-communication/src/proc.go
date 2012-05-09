4be344fa292b4410a4cf3000418598da7f763ae1
Miek Gieben <miek@miek.nl>
2012-05-10 00:17:54 +0200

    Fix this examples
    Thanks to: T.J. Yang

--- ex-communication/src/proc.go
+++ ex-communication/src/proc.go
@@ -1,23 +1,23 @@
 package main
 
-import ( "fmt"; "sort"; "exec"; "strings"; "strconv"; "container/vector")
+import (
+	"fmt"
+	"os/exec"
+	"sort"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	ps := exec.Command("ps", "-e", "-opid,ppid,comm")
 	output, _ := ps.Output()
-	child := make(map[int]*vector.IntVector)
+	child := make(map[int][]int)
 	for i, s := range strings.Split(string(output), "\n") {
-		if i == 0 || len(s) == 0 { // Kill first and last line
-			continue
-		}
+		if i == 0 || len(s) == 0 { continue } // Kill first and last line
 		f := strings.Fields(s)
 		fpp, _ := strconv.Atoi(f[1]) // the parent's pid
 		fp, _ := strconv.Atoi(f[0])  // the child's pid
-		if _, present := child[fpp]; !present {
-			v := new(vector.IntVector)
-			child[fpp] = v
-		}
-		child[fpp].Push(fp) // Save the child PIDs on a vector
+		child[fpp] = append(child[fpp], fp) // creates first one, when needed
 	}
 	schild := make([]int, len(child))
 	i := 0
@@ -27,11 +27,11 @@ func main() {
 	}
 	sort.Ints(schild)
 	for _, ppid := range schild { // Walk through the sorted list
-		fmt.Printf("Pid %d has %d child", ppid, child[ppid].Len())
-		if child[ppid].Len() == 1 {
-			fmt.Printf(": %v\n", []int(*child[ppid]))
-		} else {
-			fmt.Printf("ren: %v\n", []int(*child[ppid]))
+		fmt.Printf("Pid %d has %d child", ppid, len(child[ppid]))
+		if len(child[ppid]) == 1 {
+			fmt.Printf(": %v\n", child[ppid])
+			continue
 		}
+		fmt.Printf("ren: %v\n", child[ppid])
 	}
 }
