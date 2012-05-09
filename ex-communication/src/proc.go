8c51ae8335f881e75c7cedab8ad85dc910514371
Miek Gieben <miek@miek.nl>
2012-05-10 00:54:29 +0200

    shorter

--- ex-communication/src/proc.go
+++ ex-communication/src/proc.go
@@ -1,32 +1,22 @@
 package main
-
-import (
-	"fmt"
-	"os/exec"
-	"sort"
-	"strconv"
-	"strings"
-)
+import ( "fmt"; "os/exec"; "sort"; "strconv"; "strings")
 
 func main() {
 	ps := exec.Command("ps", "-e", "-opid,ppid,comm")
 	output, _ := ps.Output()
 	child := make(map[int][]int)
 	for i, s := range strings.Split(string(output), "\n") {
-		if i == 0 || len(s) == 0 { continue } // Kill first and last line
+		if i == 0 || len(s) == 0 { continue } // Kill first/last line
 		f := strings.Fields(s)
-		fpp, _ := strconv.Atoi(f[1]) // the parent's pid
-		fp, _ := strconv.Atoi(f[0])  // the child's pid
-		child[fpp] = append(child[fpp], fp) // creates first one, when needed
+		fpp, _ := strconv.Atoi(f[1]) // Parent's pid
+		fp, _ := strconv.Atoi(f[0])  // Child's pid
+		child[fpp] = append(child[fpp], fp)
 	}
 	schild := make([]int, len(child))
 	i := 0
-	for k, _ := range child {
-		schild[i] = k
-		i++
-	}
+	for k, _ := range child { schild[i] = k; i++ }
 	sort.Ints(schild)
-	for _, ppid := range schild { // Walk through the sorted list
+	for _, ppid := range schild {
 		fmt.Printf("Pid %d has %d child", ppid, len(child[ppid]))
 		if len(child[ppid]) == 1 {
 			fmt.Printf(": %v\n", child[ppid])
