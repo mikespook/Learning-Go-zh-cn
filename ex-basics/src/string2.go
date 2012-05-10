e8f1724a18d4ac1d6b68df6fd1cbff1f4f0eccc2
Olexandr Shalakhin <olexandr@shalakhin.com>
2012-05-10 23:54:31 +0300

    fix Lenght/Length typo

--- ex-basics/src/string2.go
+++ ex-basics/src/string2.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	str := "dsjkdshdjsdh....js"
-	fmt.Printf("String %s\nLenght: %d, Runes: %d\n", str,
+	fmt.Printf("String %s\nLength: %d, Runes: %d\n", str,
 		len([]byte(str)), utf8.RuneCount([]byte(str)))
 }
