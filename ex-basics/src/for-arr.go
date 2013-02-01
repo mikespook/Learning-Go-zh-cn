09909f1ab3587a124012e74e172ef2c82f3e29e3
Miek Gieben <miek@miek.nl>
2013-02-01 11:25:45 +0100

    layout

--- ex-basics/src/for-arr.go
+++ ex-basics/src/for-arr.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-        var arr [10]int |\coderemark{Create an array with 10 elements}|
-	for i := 0; i < 10; i++ {
-                arr[i] = i |\coderemark{Fill it one by one}|
-	}
-        fmt.Printf("%v", arr) |\coderemark{With \%v Go prints the type}|
+   var arr [10]int |\coderemark{Create an array with 10 elements}|
+   for i := 0; i < 10; i++ {
+       arr[i] = i |\coderemark{Fill it one by one}|
+   }
+   fmt.Printf("%v", arr) |\coderemark{With \%v Go prints the value for us}|
 }
