0a0729080945d4b7a30dfd61bcdb023afafd8a78
Miek Gieben <miek@miek.nl>
2013-02-02 09:02:10 +0100

    layout fixes

--- ex-functions/src/var-arg.go
+++ ex-functions/src/var-arg.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	printthem(1, 4, 5, 7, 4)
-	printthem(1, 2, 4)
+	prtthem(1, 4, 5, 7, 4)
+	prtthem(1, 2, 4)
 }
 
-func printthem(numbers ... int) { |\coderemark{\var{numbers} is now a slice of ints}|
+func prtthem(numbers... int) {|\coderemark{numbers is slice of ints}|
 	for _, d := range numbers {
 		fmt.Printf("%d\n", d)
 	}
