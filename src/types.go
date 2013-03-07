385e02846bd2e6e386023b98678cb72edbe12941
Xing Xing <mikespook@gmail.com>
2013-03-07 21:11:22 +0800

    tweaking

--- src/types.go
+++ src/types.go
@@ -1,9 +1,9 @@
 package main
 
 func main() {
-	var a int	|\coderemark{Generic integer type}|
-	var b int32	|\coderemark{32 bits integer type}|
+	var a int	|\coderemark{通用整数类型}|
+	var b int32	|\coderemark{32 位整数类型}|
 	a = 15
-	b = a + a	|\coderemark{Illegal mixing of these types}|
-	b = b + 5	|\coderemark{5 is a (typeless) constant, so this is OK}|
+	b = a + a	|\coderemark{混合这些类型是非法的}|
+	b = b + 5	|\coderemark{5 是一个（未定义类型的）常量，所以这没啥问题}|
 }
