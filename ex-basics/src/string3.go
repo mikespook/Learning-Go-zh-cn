d2715714ac66bfff2c77479bec48d4d7c2c1bbf6
Miek Gieben <miek@miek.nl>
2013-11-09 13:12:36 +0000

    Add Alexey Chernenkov and some of his proposed changes.

--- ex-basics/src/string3.go
+++ ex-basics/src/string3.go
@@ -5,11 +5,9 @@ import (
 )
 
 func main() {
-	str := "asSASA ddd dsjkdsjs dk";
-	runes := []rune(str)
-	runes[4] = rune('a')
-	runes[5] = rune('b')
-	runes[6] = rune('c')
-	fmt.Printf("Before: %s\n", str);
-	fmt.Printf("After : %s\n", string(runes))
+	s := "Шла Саша по шоссе"
+	r := []rune(s)
+	copy(r[4:4+3], []rune("abc"))
+	fmt.Printf("Before: %s\n", s);
+	fmt.Printf("After : %s\n", string(r))
 }
