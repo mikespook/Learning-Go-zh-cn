5e0ceb20537c7a132db78c9e58c6a7d278cd6bef
Bachue Zhou <bachue.shu@gmail.com>
2014-08-09 09:18:01 +0800

    Fix a small problem in code example

--- src/buffile.go
+++ src/buffile.go
@@ -9,7 +9,8 @@ func main() {
         r := bufio.NewReader(f) |\longremark{转换 \var{f} 为有缓冲的 \func{Reader}。%
 \func{NewReader} 需要一个 \type{io.Reader}，因此或许你认为这会出错。%
 但其实不会。\emph{任何}有 \func{Read()} 函数就实现了这个接口。%
-同时，从列表\ref{src:read}可以看到，\type{*os.File} 已经这样做了；}|	w := bufio.NewWriter(os.Stdout)
+同时，从列表\ref{src:read}可以看到，\type{*os.File} 已经这样做了；}|	
+	w := bufio.NewWriter(os.Stdout)
 	defer w.Flush()
 	for {
                 n, _ := r.Read(buf)     |\longremark{从 Reader 读取，而向 Writer 写入，然后%
