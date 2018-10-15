808af95ded5eb68bc89e1e7802772c48e4129cd0
Xing Xing <mikespook@gmail.com>
2018-10-15 15:49:18 +1300

    format src

--- src/basic/helloworld.go
+++ src/basic/helloworld.go
@@ -1,10 +1,13 @@
 package main //<<1>>
 
-import "fmt" //<<2>> // 格式化输出包
+import "fmt" //<<2>> // 引用格式化输出
 
 /* Print something */ //<<3>>
 
 func main() {
-	文字 := "Hello, world or こんにちは 世界" //<<5>>
-	fmt.Println(文字)                  //<<6>>
+	文字 := "Hello, world!\n" +
+		"καλημέρα κóσμε!" +
+		"こんにちは 世界!" +
+		"你好，世界！" //<<5>>
+	fmt.Println(文字) //<<6>>
 }
